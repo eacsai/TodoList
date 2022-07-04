381ba4605ecfd35cecb5279d1f725c972c8f8e60
eacsai <1349157283@qq.com>
2022-07-04 16:41:47 +0800

    delete dist

--- modern.config.ts
+++ modern.config.ts
@@ -1,4 +1,10 @@
 import { defineConfig } from '@modern-js/app-tools';
 
 // https://modernjs.dev/docs/apis/config/overview
-export default defineConfig({});
+export default defineConfig({
+  output: {
+    path: 'docs',
+    title: 'TodoList',
+    favicon: './src/assets/favicon.ico',
+  },
+});
